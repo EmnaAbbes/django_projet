c83b51a1a485c062c29e97bcdc7ab1e686a45ab2
mohamedba26 <122885497+mohamedba26@users.noreply.github.com>
2023-05-19 16:30:35 +0100

    new commit1

---
+++ ArtyProd/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Service(models.Model):
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+    def __str__(self):
+        return self.type
+
+
+class Projet(models.Model):
+    libelle = models.CharField(max_length=100)
+    description = models.TextField()
+    date_debut = models.DateField()
+    date_fin = models.DateField()
+    acheve = models.CharField(max_length=1, choices=(('o', 'Oui'), ('n', 'Non')))
+    def __str__(self):
+        return self.libelle
+
+
+class Details(models.Model):
+    fichier = models.FileField(upload_to='details/')
+    serviceID = models.ForeignKey(Service, on_delete=models.CASCADE)
+    projetID = models.ForeignKey(Projet, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.fichier.name
+
+class Equipe(models.Model):
+    projetID=models.OneToOneField(Projet, on_delete=models.CASCADE)
+
+class Personnel(models.Model):
+    nom = models.CharField(max_length=100)
+    fichier_CV = models.FileField(upload_to='personnel/cv/')
+    fichier_photo = models.ImageField(upload_to='personnel/photos/')
+    lien_linkedIn = models.URLField(blank=True)
+    equipeID=models.ForeignKey(Equipe, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nom
