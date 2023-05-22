e8e3c20bfe87a5a676c297e0966d4fc27b811367
emna.abbes5 <emna.abbes5@gmail.com>
2023-05-22 19:13:53 +0100

    new commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Personnel)
+admin.site.register(Equipe)
+admin.site.register(Projet)
+admin.site.register(Service)
+admin.site.register(Details)
 
-# Register your models here.
---
+++ myapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from . import views
+urlpatterns = [
+path('', views.index,name="index"),
+path('services/',views.services,name="services")
+]
