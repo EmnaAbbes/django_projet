13d151674f968855d2e5730e00d9d78d813dee5a
emna.abbes5 <emna.abbes5@gmail.com>
2023-05-23 12:45:55 +0100

    new commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
 path('', views.index,name="index"),
-path('services/',views.services,name="services")
+path('services/',views.services,name="services"),
+path('team/',views.team,name="team"),
+path('project/',views.project,name="project"),
+path('details/<int:id>/',views.details,name="details"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404
+from .models import Service,Personnel,Projet,Details
 def index(request):
     return render(request,'myapp/index.html')
 def services(request):
-    return render(request,'myapp/services.html')
+    list = Service.objects.all()
+    return render(request,'myapp/services.html',{'list':list})
+def team(request):
+    list = Personnel.objects.all()
+   
+    return render(request,'myapp/team.html',{'list':list})
+def project(request):
+    list = Projet.objects.all()
+    return render(request,'myapp/project.html',{'list':list})
+def details(request,id):
+    list = get_object_or_404(Details, projetID=id)
+    return render(request,'myapp/projectDetails.html',{'list':list})
